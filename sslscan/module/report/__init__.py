634f8dea45bf1667832594fdce5d9d479401ccfd
PhiBo <phibo@dinotools.org>
2014-06-27 18:34:36 +0200

    src - Use a function to determine all rating mods

--- sslscan/module/report/__init__.py
+++ sslscan/module/report/__init__.py
@@ -1,12 +1,33 @@
 from sslscan.module import BaseModule
+from sslscan.module.rating import BaseRating
+
+
+def get_rating_option_values(option):
+    config = option.get_parent()
+    module = config._module
+    scanner = module.get_scanner()
+
+    mod_mgr = scanner.get_module_manager()
+
+    modules = mod_mgr.get_modules(base_class=BaseRating)
+
+    values = []
+    for module in modules:
+        values.append(module.name)
+
+    return values
+
 
 class BaseReport(BaseModule):
     config_options = BaseModule.config_options + [
         (
             "rating", {
                 "help": "",
+                "type": "choice",
+                "values": get_rating_option_values
             }
         )
     ]
+
     def __init__(self, **kwargs):
         BaseModule.__init__(self, **kwargs)
