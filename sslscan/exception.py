c26f1595eb6909c48b86505ad1efeb9ae0e8b778
PhiBo <phibo@dinotools.org>
2014-06-27 18:25:22 +0200

    src - Add exceptions for config errors

--- sslscan/exception.py
+++ sslscan/exception.py
@@ -5,3 +5,21 @@ class ModuleNotFound(Exception):
 
     def __str__(self):
         return "Module '{}' not found".format(self.name)
+
+
+class BaseConfigError(Exception):
+    pass
+
+
+class OptionValueError(BaseConfigError):
+    def __init__(self, option, value):
+        self.option = option
+        self.value = value
+
+    def __str__(self):
+        s = "Unable to set value '{2}' for option '{0}' of type '{1}'".format(
+            self.option.name,
+            self.option.type,
+            self.value
+        )
+        return s
