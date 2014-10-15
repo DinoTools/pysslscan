273e80103a5703f76b2824c0bd41c614db8af766
PhiBo <phibo@dinotools.org>
2014-10-15 22:38:55 +0200

    src - Use flextls to scan for server ciphers
    * Don't do a full handshake (refs #1)
    * Limit number of ClientHello packages (refs #2)
    
    This is a proof of concept.

--- sslscan/module/scan/server_ciphers.py
+++ sslscan/module/scan/server_ciphers.py
@@ -1,4 +1,7 @@
-from OpenSSL import SSL, _util
+from flextls.field import CipherSuiteField, CompressionMethodField
+from flextls.protocol.handshake import ClientHello, Handshake, ServerHello
+from flextls.protocol.handshake.extension import EllipticCurves, SignatureAlgorithms, Extension, SessionTicketTLS
+from flextls.protocol.record import RecordSSLv3
 
 from sslscan import modules
 from sslscan.kb import Cipher
@@ -16,54 +19,79 @@ class ServerCiphers(BaseScan):
         BaseScan.__init__(self, **kwargs)
 
     def run(self):
-        ciphers = []
-        for method in self._scanner.get_enabled_methods():
-            try:
-                ctx = SSL.Context(method)
-            except:
-                # ToDo:
-                continue
-            conn = SSL.Connection(ctx)
-            ctx.set_cipher_list("ALL:COMPLEMENT")
-            cipher_ptr = _util.lib.SSL_get_ciphers(conn._ssl)
-            for i in range(_util.lib.sk_SSL_CIPHER_num(cipher_ptr)):
-                cipher = _util.lib.sk_SSL_CIPHER_value(cipher_ptr, i)
-                ciphers.append(
-                    Cipher(
-                        bits=_util.lib.SSL_CIPHER_get_bits(
-                            cipher,
-                            _util.ffi.NULL
-                        ),
-                        method=method,
-                        name=_util.ffi.string(_util.lib.SSL_CIPHER_get_name(cipher))
-                    )
-                )
-
         kb = self._scanner.get_knowledge_base()
 
-        for cipher in ciphers:
-            conn = self._scanner.handler.connect()
-            # ToDo: error handling
-            ctx = SSL.Context(cipher.method)
-            ctx.set_cipher_list(cipher.name)
-            conn_ssl = SSL.Connection(ctx, conn)
-            #print(conn_ssl.get_cipher_list())
-            conn_ssl.set_connect_state()
+        versions = []
+        if self._scanner.config.get_value('ssl2'):
+            versions.append((2, 0))
+        if self._scanner.config.get_value('ssl3'):
+            versions.append((3, 0))
+        if self._scanner.config.get_value('tls10'):
+            versions.append((3, 1))
+        if self._scanner.config.get_value('tls11'):
+            versions.append((3, 2))
+        if self._scanner.config.get_value('tls12'):
+            versions.append((3, 3))
 
-            cipher_status = _util.lib.SSL_do_handshake(conn_ssl._ssl)
+        for ver_major, ver_minor in versions:
+            cipher_suites = [i for i in range(0, 0x1000)]
+            while True:
+                conn = self._scanner.handler.connect()
 
-            if cipher_status < 0:
-                cipher.status = -1
-            elif cipher_status == 0:
-                cipher.status = 0
-            elif cipher_status == 1:
-                cipher.status = 1
+                hello = ClientHello()
 
-            # ToDo: handle alerts
+                for i in cipher_suites:
+                    cipher = CipherSuiteField()
+                    cipher.value = i
+                    hello.cipher_suites.append(cipher)
 
-            kb.append('server.ciphers', cipher)
+                comp = CompressionMethodField()
+                comp.value = 0
 
-            conn_ssl.close()
+                hello.compression_methods.append(comp)
+                ext_elliptic_curves = EllipticCurves()
+                a = ext_elliptic_curves.get_field("elliptic_curve_list")
+                for i in range(1, 25):
+                    v = a.item_class("unnamed", None)
+                    v.value = i
+                    a.value.append(v)
+
+                hello.extensions.append(Extension() + ext_elliptic_curves)
+
+                ext_signature_algorithm = SignatureAlgorithms()
+                a = ext_signature_algorithm.get_field("supported_signature_algorithms")
+                for i in range(0, 7):
+                    for j in range(0, 4):
+                        v = a.item_class("unnamed")
+                        v.hash = i
+                        v.signature = j
+                        a.value.append(v)
+
+                hello.extensions.append(Extension() + ext_signature_algorithm)
+
+                hello.extensions.append(Extension() + SessionTicketTLS())
+
+                msg_hello = RecordSSLv3() + (Handshake() + hello)
+                msg_hello.payload.payload.random.random_bytes = b"A"*32
+                msg_hello.version.minor = ver_minor
+                msg_hello.payload.payload.version.minor = ver_minor
+                conn.send(msg_hello.encode())
+                data = conn.recv(4096)
+                (record, data) = RecordSSLv3.decode(data)
+                if isinstance(record.payload.payload, ServerHello):
+                    server_hello = record.payload.payload
+                    kb.append(
+                        'server.ciphers',
+                        Cipher(
+                            bits=0,
+                            method="TLS %d.%d" % (ver_major, ver_minor),
+                            name=server_hello.cipher_suite,
+                            status=1
+                        )
+                    )
+                    cipher_suites.remove(server_hello.cipher_suite)
+                else:
+                    break
 
 
 modules.register(ServerCiphers)
