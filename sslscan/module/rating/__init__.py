3bc6df4b6fffcc0f52080d2b959882549bbff656
PhiBo <phibo@dinotools.org>
2014-11-24 23:09:49 +0100

    src/rating - Test extended information support

--- sslscan/module/rating/__init__.py
+++ sslscan/module/rating/__init__.py
@@ -6,20 +6,63 @@ class BaseRating(BaseModule):
         BaseModule.__init__(self, **kwargs)
         self._rules = {}
 
-    def rate(self, rule_id, data):
-        rules = self._rules.get(rule_id)
-        if rules is None:
-            return None
+    def add_rule(self, rule):
+        name = rule.name
+        self._rules[name] = rule
 
-        for rule in rules:
-            result = rule(data)
-            if result is not None:
-                return result
-        return result
+    def get_rule(self, name):
+        return self._rules.get(name)
+
+    def rate(self, rule_name, value):
+        rule = self.get_rule(rule_name)
+        if rule is None:
+            return None
+        return rule.rate(value)
+
 
 class NoneRating(BaseRating):
     name="none"
 
     def __init__(self, **kwargs):
         BaseModule.__init__(self, **kwargs)
-        self._rules = {}
+
+
+class RatingRule(object):
+    def __init__(self, name, description="", result_description=None,
+            rules=[]):
+        self.name = name
+        self.description = description
+        self._result_description = result_description
+        self._rules = rules
+
+    def rate(self, value):
+        result = None
+        for rule in self._rules:
+            result = rule(value)
+            if result is not None:
+                break
+        return RatingResult(result, self)
+
+
+class RatingResult(object):
+    def __init__(self, result, rule):
+        self._result = result
+        self._rule = rule
+
+    def __eq__(self, other):
+        return self.get_value() == other
+
+    def __gt__(self, other):
+        return self.get_value() > other
+
+    def __lt__(self, other):
+        return self.get_value() < other
+
+    def get_description(self):
+        return self._rule.description
+
+    def get_result_description(self):
+        return self._rule._result_description.get(self._result)
+
+    def get_value(self):
+        return self._result
