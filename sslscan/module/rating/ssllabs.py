d05645775cda16b4f05fde6f060874f43a076baf
PhiBo <phibo@dinotools.org>
2014-11-24 23:16:20 +0100

    src/rating - Use improved rating support

--- sslscan/module/rating/ssllabs.py
+++ sslscan/module/rating/ssllabs.py
@@ -4,7 +4,7 @@ from socket import socket
 from flextls import registry as reg
 
 from sslscan import modules
-from sslscan.module.rating import BaseRating
+from sslscan.module.rating import BaseRating, RatingRule
 
 
 class SSLLabs2009c(BaseRating):
@@ -18,22 +18,49 @@ class SSLLabs2009c(BaseRating):
 
     def __init__(self, **kwargs):
         BaseRating.__init__(self, **kwargs)
-        self._rules.update({
-            "cipher.bits": [
-                lambda bits: 6 if bits == 0 else None,
-                lambda bits: 5 if bits < 128 else None,
-                lambda bits: 2 if bits < 256 else None,
-                lambda bits: 1 if bits >= 256 else None
-            ],
-            "cipher.protocol_version": [
-                lambda method: 6 if method == reg.version.SSLv2 else None,
-                lambda method: 1 if method == reg.version.TLSv12 else None,
-            ],
-            "server.renegotiation.secure": [
-                lambda status: 6 if status == False else None,
-                lambda status: 1 if status == True else None
-            ]
-        })
+        self.add_rule(
+            RatingRule(
+                "cipher.bits",
+                rules=[
+                    lambda bits: 6 if bits == 0 else None,
+                    lambda bits: 5 if bits < 128 else None,
+                    lambda bits: 2 if bits < 256 else None,
+                    lambda bits: 1 if bits >= 256 else None
+                ]
+            )
+        )
+
+        self.add_rule(
+            RatingRule(
+                "cipher.bits",
+                rules=[
+                    lambda bits: 6 if bits == 0 else None,
+                    lambda bits: 5 if bits < 128 else None,
+                    lambda bits: 2 if bits < 256 else None,
+                    lambda bits: 1 if bits >= 256 else None
+                ]
+            )
+        )
+
+        self.add_rule(
+            RatingRule(
+                "cipher.protocol_version",
+                rules=[
+                    lambda method: 6 if method == reg.version.SSLv2 else None,
+                    lambda method: 1 if method == reg.version.TLSv12 else None,
+                ]
+            )
+        )
+
+        self.add_rule(
+            RatingRule(
+                "server.renegotiation.secure",
+                rules=[
+                    lambda status: 6 if status == False else None,
+                    lambda status: 1 if status == True else None
+                ]
+            )
+        )
 
 
 class SSLLabs2009d(BaseRating):
@@ -47,22 +74,37 @@ class SSLLabs2009d(BaseRating):
 
     def __init__(self, **kwargs):
         BaseRating.__init__(self, **kwargs)
-        self._rules.update({
-            "cipher.bits": [
-                lambda bits: 6 if bits == 0 else None,
-                lambda bits: 5 if bits < 128 else None,
-                lambda bits: 2 if bits < 256 else None,
-                lambda bits: 1 if bits >= 256 else None
-            ],
-            "cipher.protocol_version": [
-                lambda method: 6 if method == reg.version.SSLv2 else None,
-                lambda method: 1 if method == reg.version.TLSv12 else None,
-            ],
-            "server.renegotiation.secure": [
-                lambda status: 6 if status == False else None,
-                lambda status: 1 if status == True else None
-            ]
-        })
+        self.add_rule(
+            RatingRule(
+                "cipher.bits",
+                rules=[
+                    lambda bits: 6 if bits == 0 else None,
+                    lambda bits: 5 if bits < 128 else None,
+                    lambda bits: 2 if bits < 256 else None,
+                    lambda bits: 1 if bits >= 256 else None
+                ]
+            )
+        )
+
+        self.add_rule(
+            RatingRule(
+                "cipher.protocol_version",
+                rules=[
+                    lambda method: 6 if method == reg.version.SSLv2 else None,
+                    lambda method: 1 if method == reg.version.TLSv12 else None,
+                ]
+            )
+        )
+
+        self.add_rule(
+            RatingRule(
+                "server.renegotiation.secure",
+                rules=[
+                    lambda status: 6 if status == False else None,
+                    lambda status: 1 if status == True else None
+                ]
+            )
+        )
 
 
 class SSLLabs2009e(BaseRating):
@@ -76,32 +118,65 @@ class SSLLabs2009e(BaseRating):
 
     def __init__(self, **kwargs):
         BaseRating.__init__(self, **kwargs)
-        self._rules.update({
-            "cipher.bits": [
-                lambda bits: 6 if bits == 0 else None,
-                lambda bits: 5 if bits < 128 else None,
-                lambda bits: 3 if bits < 256 else None,
-                lambda bits: 0 if bits >= 256 else None
-            ],
-            "cipher.protocol_version": [
-                lambda method: 6 if method == reg.version.SSLv2 else None,
-                lambda method: 1 if method == reg.version.TLSv12 else None,
-            ],
-            "server.certificate.x509.signature_algorithm": [
-                lambda algorithm: 6 if algorithm.startswith("md2") else None,
-                lambda algorithm: 6 if algorithm.startswith("md5") else None,
-            ],
-            "server.certificate.x509.not_after": [
-                lambda date: 6 if date < datetime.now() else None
-            ],
-            "server.certificate.x509.not_before": [
-                lambda date: 6 if date > datetime.now() else None
-            ],
-            "server.renegotiation.secure": [
-                lambda status: 6 if status == False else None,
-                lambda status: 1 if status == True else None
-            ]
-        })
+        self.add_rule(
+            RatingRule(
+                "cipher.bits",
+                rules=[
+                    lambda bits: 6 if bits == 0 else None,
+                    lambda bits: 5 if bits < 128 else None,
+                    lambda bits: 3 if bits < 256 else None,
+                    lambda bits: 0 if bits >= 256 else None
+                ]
+            )
+        )
+
+        self.add_rule(
+            RatingRule(
+                "cipher.protocol_version",
+                rules=[
+                    lambda method: 6 if method == reg.version.SSLv2 else None,
+                    lambda method: 1 if method == reg.version.TLSv12 else None,
+                ]
+            )
+        )
+
+        self.add_rule(
+            RatingRule(
+                "server.certificate.x509.signature_algorithm",
+                rules=[
+                    lambda algorithm: 6 if algorithm.startswith("md2") else None,
+                    lambda algorithm: 6 if algorithm.startswith("md5") else None,
+                ]
+            )
+        )
+
+        self.add_rule(
+            RatingRule(
+                "server.certificate.x509.not_after",
+                rules=[
+                    lambda date: 6 if date < datetime.now() else None
+                ],
+            )
+        )
+
+        self.add_rule(
+            RatingRule(
+                "server.certificate.x509.not_before",
+                rules=[
+                    lambda date: 6 if date > datetime.now() else None
+                ],
+            )
+        )
+
+        self.add_rule(
+            RatingRule(
+                "server.renegotiation.secure",
+                rules=[
+                    lambda status: 6 if status == False else None,
+                    lambda status: 1 if status == True else None
+                ]
+            )
+        )
 
 
 modules.register(SSLLabs2009c)
