776129e156968eb8f30fd5220f2bec3a59bef687
PhiBo <phibo@dinotools.org>
2015-02-04 21:54:49 +0100

    src - Bump version to 0.5dev1

--- sslscan/__about__.py
+++ sslscan/__about__.py
@@ -13,10 +13,10 @@ __title__ = "sslscan"
 __summary__ = "Framework and command-line tool to scan SSL enabled services"
 __uri__ = "https://github.com/DinoTools/pysslscan"
 
-__version__ = "0.4"
+__version__ = "0.5dev1"
 
 __author__ = "PhiBo (DinoTools)"
 __email__ = ""
 
 __license__ = "LGPLv3+"
-__copyright__ = "Copyright 2014 %s" % __author__
+__copyright__ = "Copyright 2014-2015 %s" % __author__
