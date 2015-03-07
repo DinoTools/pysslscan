2dd4b330787f7f21b6a10f922347914b1e5243d4
PhiBo <phibo@dinotools.org>
2015-03-07 08:03:21 +0100

    doc - Add mocks to fix build errors if six is not installed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['cffi', 'OpenSSL']
+MOCK_MODULES = ['cffi', 'OpenSSL', 'six', 'six.moves.urllib.parse']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
