35c9123a9db5d2d9be64406ad05059f41b212850
PhiBo <phibo@dinotools.org>
2015-07-20 23:51:44 +0200

    build - Update dependencies

--- setup.py
+++ setup.py
@@ -38,6 +38,9 @@ setup(
         "flextls >= 0.3",
         "six >= 1.4.1"
     ],
+    extras_require={
+        "terminal": ["colorama >= 0.2.4"]
+    },
     packages=find_packages(exclude=["*.tests", "*.tests.*"]),
     include_package_data=True,
     package_data={
